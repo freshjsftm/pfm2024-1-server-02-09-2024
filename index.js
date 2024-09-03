acd9b3cb5c32e6ba9c3cba54f5ae0f6c87c72761
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-09-03 11:53:05 +0300

    Refactor routerGET

--- index.js
+++ index.js
@@ -1,62 +1,45 @@
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs').promises;
 const PORT = 3000;
 const users = [];
-const server = http.createServer((req, res) => {
+const routerGET = {
+  '/': './views/index.html',
+  '/about': './views/about.html',
+  '/contact': './views/contact.html',
+};
+const requestListener = async (req, res) => {
   const { method, url } = req;
   if (method === 'GET') {
-    if (url === '/') {
-      fs.readFile('./views/index.html', { encoding: 'utf8' }, (err, data) => {
-        if (err) {
-          throw err;
-        }
-        res.end(data);
-      });
-      return;
-    }
-    if (url === '/about') {
-      fs.readFile('./views/about.html', { encoding: 'utf8' }, (err, data) => {
-        if (err) {
-          throw err;
-        }
-        res.end(data);
-      });
-      return;
-    }
-    if (url === '/contact') {
-      fs.readFile('./views/contact.html', { encoding: 'utf8' }, (err, data) => {
-        if (err) {
-          throw err;
-        }
-        res.end(data);
-      });
-      return;
+    for (const path in router) {
+      if (url === path) {
+        const data = await fs.readFile(routerGET[path], 'utf8');
+        return res.end(data);
+      }
     }
   }
-  if(method === 'POST'){
-    if(url === '/users'){
+  if (method === 'POST') {
+    if (url === '/users') {
       let jsonStr = '';
-      req.on('data', (chunk)=>{
+      req.on('data', (chunk) => {
         jsonStr += chunk;
-      })    
-      req.on('end', ()=>{
+      });
+      req.on('end', () => {
         const user = JSON.parse(jsonStr);
         delete user.password;
         user.id = Date.now();
         users.push(user);
         console.log(users);
-        res.end(JSON.stringify(user));       
-      })
+        res.end(JSON.stringify(user));
+      });
       return;
     }
   }
-  fs.readFile('./views/404.html', { encoding: 'utf8' }, (err, data) => {
-    if (err) {
-      throw err;
-    }
-    res.end(data);
-  });
-});
+
+  const data = await fs.readFile('./views/404.html', 'utf8');
+  res.end(data);
+};
+
+const server = http.createServer(requestListener);
 
 server.listen(PORT, () => {
   console.log('server start at port ' + PORT);
