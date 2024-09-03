7c6019d430b50f67aefb48dd14d14307a2be269a
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-09-03 09:52:49 +0300

    Add createServer

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hi');
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  // console.log('req', req);
+  // console.log('res', res);
+  res.end('hello from server!');
+});
+
+server.listen(PORT, () => {
+  console.log('server start at port ' + PORT);
+});
